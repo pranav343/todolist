dd9df70f3bbbf121e1b2aed3f7334362d0511212
pranav343 <pranav.dhiman334433@gmail.com>
2023-06-14 23:17:08 +0530

    port changed

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.post("/", function (req, res) {
   
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("server started on port 3000");
 });
 
